a36637017c03be1f0ed62e3162a6d8262fbe26a4
Jared Steele <jasteele@mines.edu>
2011-09-18 14:52:23 -0600

    eucleadian distance implemented, not normalized

---
+++ project04/similartiy_metrics/euclideanDistance.rb
@@ -0,0 +1,18 @@
+# Euclidean distance function.
+# Returns the linear distance between two objects this and that, represented as
+# a floating point value between 0 and 1.
+# Passing in arrays of values
+def euclidean_distance(this,that)
+	# make sure that the arrays are same length (same # of attributes)
+  if this.length == that.length
+    sum = 0
+    # for each dimension calculate the distance between this and that
+    this.each_with_index do |x,index|
+      sum += (x - that[index])**2
+    end
+    distance = Math.sqrt(sum)
+  else
+    puts "Arrays of differing length provided"
+    return nil
+  end
+end
