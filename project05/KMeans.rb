206700bbd4866eda79bbce4270a16773790c9959
Jared Steele <jasteele@mines.edu>
2011-09-27 18:00:44 -0600

    Loading of csv file successful, and basic class structure established.

--- project05/KMeans.rb
+++ project05/KMeans.rb
@@ -1,4 +1,50 @@
 class KMeans
-	
-	
+  attr_accessor :k
+
+  def initialize(k)
+    @k = k
+    @allpoints = []
+  end
+  
+  def load(fileName)
+    entries = []
+    data = File.open(fileName, "r")
+    data.each_line("\n") do |row|
+      columns = row.split(",")
+      if columns.length == 5
+        @allpoints.push(Iris.new(columns[0],columns[1],columns[2],columns[3]))
+      end
+    end
+  end   
+end
+
+class Cluster
+  attr_accessor :location, :points
+  
+  def initialize(location)
+    @location = location
+    @points = []
+  end
+
+  def move!
+
+  end
+end
+
+# class to hold the csv data from the file
+class Iris
+  attr_accessor :a, :b, :c, :d
+  
+  def initialize(a,b,c,d)
+    @a = a
+    @b = b
+    @c = c
+    @d = d
+  end
+
+  def to_s
+    puts "#{@a}, #{@b}, #{@c}, #{@d}"
+  end
+
+
 end
