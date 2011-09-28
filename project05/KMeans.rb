3b9f9cb3da1d1d5774353ffe06d65e5735e754b3
Jared Steele <jasteele@mines.edu>
2011-09-28 16:27:51 -0600

    kmeans now correctly implemented, begining SSE method

--- project05/KMeans.rb
+++ project05/KMeans.rb
@@ -1,6 +1,6 @@
 #Jared Steele's KMeans implementation for the iris dataset
 class KMeans
-  attr_accessor :k
+  attr_accessor :k, :allPoints, :allClusters
 
   def initialize(k)
     @k = k
@@ -14,39 +14,41 @@ class KMeans
     (1..@k).each do |i|
       @allClusters.push(Cluster.new([rand(@maxs[0]),rand(@maxs[1]),rand(@maxs[1]),rand(@maxs[1])]))
     end
+    
+    while true
+      # assign each point to the nearest centroid (cluster)
+      @allPoints.each do |point|
+        closestCluster = nil
+        distanceToNearestCluster = 10000 #larger than the distance between any point will be
 
-    # assign each point to the nearest centroid (cluster)
-    @allPoints.each do |point|
-      closestCluster = nil
-      distanceToNearestCluster = 10000 #larger than the distance between any point will be
+        @allClusters.each do |cluster|
+          distance = EuclideanDistance.euclideanDistance(point,cluster.location)
 
-      @allClusters.each do |cluster|
-        distance = point.EuclideanDistance.euclideanDistance(point,cluster.location)
-
-        if distance < distanceToNearestCluster
-          distanceToNearestCluster = dist
-          closestCluster = cluster
+          if distance < distanceToNearestCluster
+            distanceToNearestCluster = distance
+            closestCluster = cluster
+          end
         end
+
+        closestCluster.points.push(point)
       end
 
-      closestCluster.points.push(point)
+      #move the centroids and stop if they have barely moved
+      movement = 0.0
+      @allClusters.each do |cluster|
+        movement += cluster.move!
+        puts movement
+      end
+
+      if movement == 0.0
+        return @allClusters
+      end 
+
+      # clear the points in the cluster 
+      @allClusters.each do |cluster|
+        cluster.points = []
+      end  
     end
-
-    #move the centroids and stop if they have barely moved
-    movement = 0.0
-    @allClusters.each do |cluster|
-      movement += cluster.move!
-      puts movement
-    end
-
-    if movement < .1
-      return @allClusters
-    end 
-
-    # clear the points in the cluster 
-    @allClusters.each do |cluster|
-      cluster.points = []
-    end  
   end
 
   def load(fileName)
@@ -59,7 +61,7 @@ class KMeans
             @maxs[index] = columns[index].to_f
           end
         end
-        @allPoints.push([columns[0],columns[1],columns[2],columns[3]])
+        @allPoints.push([columns[0].to_f, columns[1].to_f, columns[2].to_f, columns[3].to_f])
       end
     end
   end   
@@ -72,25 +74,26 @@ class Cluster
   def initialize(location)
     @location = location
     @points = []
-    @distanceMoved = 10
+    @distanceMoved
   end
 
   def move!
     a = b = c = d = 0
     oldLocation = @location
-    @points.each do |iris|
-      a += iris[0]
-      b += iris[1]
-      c += iris[2]
-      d += iris[3]
+    if(@points.length != 0)
+      @points.each do |iris|
+        a += iris[0]
+        b += iris[1]
+        c += iris[2]
+        d += iris[3]
+      end
+
+      a /= @points.length
+      b /= @points.length
+      c /= @points.length
+      d /= @points.length
+      @location = [a,b,c,d]
     end
-
-    a /= points.length
-    b /= points.length
-    c /= points.length
-    d /= points.length
-
-    @location = [a,b,c,d]
     @distanceMoved = EuclideanDistance.euclideanDistance(oldLocation, @location)
   end
 end
