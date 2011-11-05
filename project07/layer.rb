643303829359fb4d1f3de75a9e308efd8f624740
Jared Steele <jasteele@mines.edu>
2011-11-04 18:19:44 -0600

    ok totally messed up part of this because my brain was not working, hopefully have time to fix it before midnight...

--- project07/layer.rb
+++ project07/layer.rb
@@ -1,11 +1,12 @@
 class Layer
 	attr_accessor :neurons, :forward_layer
 	def initialize(numNeurons, forward_layer)
+		@forward_layer = forward_layer
 		@neurons = []
 		(0...numNeurons).each do |index|
 			@neurons[index] = Neuron.new
 		end
-		@forward_layer = forward_layer
+		
 	end
 
 end
